0523f7276e17aecab57dd4e9d4938c400679cf53
Erik G. H. Meade <github-m@eghm.net>
2012-10-11 15:42:24 -0700

    make sure -Dremote.driver.saucelabs param is not null"

--- bin/mvnSmokeTest.sh
+++ bin/mvnSmokeTest.sh
@@ -8,4 +8,4 @@ then
 fi
 
 mvn-log.sh -f sampleapp/pom.xml test-compile
-mvn-log.sh -f sampleapp/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs -Dremote.driver.saucelabs.user=$2 -Dremote.driver.saucelabs.key=$3 -Dremote.driver.saucelabs.version=$4 -Dremote.driver.saucelabs.platform=$5 -Dremote.driver.saucelabs.browser=$6 -Dremote.public.userpool=$7 -Dit.test=$RUNNING_TEST
+mvn-log.sh -f sampleapp/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs=defined -Dremote.driver.saucelabs.user=$2 -Dremote.driver.saucelabs.key=$3 -Dremote.driver.saucelabs.version=$4 -Dremote.driver.saucelabs.platform=$5 -Dremote.driver.saucelabs.browser=$6 -Dremote.public.userpool=$7 -Dit.test=$RUNNING_TEST
