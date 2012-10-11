91c6731a22fe965b009291469e59280719ed52d5
Erik G. H. Meade <github-m@eghm.net>
2012-10-11 00:41:51 -0700

    echos

--- bin/updateRunningTest.sh
+++ bin/updateRunningTest.sh
@@ -2,3 +2,5 @@ echo "Getting test to run from pool..."
 echo "curl -o namepooltest.json http://testuserpool.appspot.com/namepool?key=1"
 curl -o namepooltest.json http://testuserpool.appspot.com/namepool?key=1 
 cat namepooltest.json | JSON.sh | grep -F -e "[\"name\"]" | cut -s -f 4 -d\" > RUNNING_TEST
+export RUNNING_TEST=$(cat RUNNING_TEST)
+echo "Going to run $RUNNING_TEST"
