bd7eda426ad8a7980d81c8fe2fdac1a1cf80f61c
Erik G. H. Meade <github-m@eghm.net>
2012-10-11 00:49:33 -0700

    apply sauce labs patch if it hasnt been yet

--- bin/mvnSmokeTest.sh
+++ bin/mvnSmokeTest.sh
@@ -1,5 +1,11 @@
 #export DTS=$(date +%Y%m%d%H%M)
 #export MAVEN_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m"
 
+if [ ! -e sampleapp/src/it/java/edu/samplu/common/SauceLabsWebDriverHelper.java ]
+then
+	echo "Patching for Sauce Labs"
+	patch -p0 < ../rtools/etc/SauceLabs.patch
+fi
+
 mvn-named-log.sh $RUNNING_TEST.test-compile.out -f sampleapp/pom.xml test-compile
 mvn-named-log.sh $RUNNING_TEST.out -f sampleapp/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs -Dremote.driver.saucelabs.user=$2 -Dremote.driver.saucelabs.key=$3 -Dremote.driver.saucelabs.version=$4 -Dremote.driver.saucelabs.platform=$5 -Dremote.driver.saucelabs.browser=$6 -Dremote.public.userpool=$7
