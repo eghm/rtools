e7c9ffc5f22835a3dac94ada826b95f1f9940d1f
Erik G. H. Meade <github-m@eghm.net>
2012-10-11 17:50:21 -0700

    hit the 10 arguments limit

--- bin/mvnSmokeTest.sh
+++ bin/mvnSmokeTest.sh
@@ -1,4 +1,4 @@
-#export DTS=$(date +%Y%m%d%H%M)
+export DTS=$(date +%Y%m%d%H%M)
 #export MAVEN_OPTS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m"
 
 if [ ! -e sampleapp/src/it/java/edu/samplu/common/SauceLabsWebDriverHelper.java ]
@@ -8,4 +8,11 @@ then
 fi
 
 ~/rtools/bin/mvn-log.sh -f sampleapp/pom.xml test-compile
-~/rtools/bin/mvn-log.sh -f sampleapp/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs=defined -Dremote.driver.saucelabs.user=$2 -Dremote.driver.saucelabs.key=$3 -Dremote.driver.saucelabs.version=$4 -Dremote.driver.saucelabs.platform=$5 -Dremote.driver.saucelabs.browser=$6 -Dremote.public.userpool=$7 -Dit.test=$RUNNING_TEST
+
+export logname=st-$RUNNING_TEST
+touch $R_HOME/logs/$rDir/$logname.$DTS.out 
+ln -s $R_HOME/logs/$rDir/$logname.$DTS.out $logname.$DTS.out
+mvn -version  >> $logname.$DTS.out
+
+echo "mvn -f sampleapp/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs=defined -Dremote.driver.saucelabs.user=$2 -Dremote.driver.saucelabs.key=$3 -Dremote.driver.saucelabs.version=$4 -Dremote.driver.saucelabs.platform=$5 -Dremote.driver.saucelabs.browser=$6 -Dremote.public.userpool=$7 -Dit.test=$RUNNING_TEST" >> $logname.$DTS.out
+mvn -f sampleapp/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs=defined -Dremote.driver.saucelabs.user=$2 -Dremote.driver.saucelabs.key=$3 -Dremote.driver.saucelabs.version=$4 -Dremote.driver.saucelabs.platform=$5 -Dremote.driver.saucelabs.browser=$6 -Dremote.public.userpool=$7 -Dit.test=$RUNNING_TEST >> $logname.$DTS.out
