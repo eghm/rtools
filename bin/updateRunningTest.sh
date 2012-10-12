a45a273ca37169b7b826708fd414ec2b2afd84d5
Erik G. H. Meade <github-m@eghm.net>
2012-10-11 18:41:51 -0700

    path fix

--- bin/updateRunningTest.sh
+++ bin/updateRunningTest.sh
@@ -1,6 +1,6 @@
 echo "Getting test to run from pool..."
 echo "curl -o namepooltest.json http://testuserpool.appspot.com/namepool?key=1"
 curl -o namepooltest.json http://testuserpool.appspot.com/namepool?key=1 
-cat namepooltest.json | JSON.sh | grep -F -e "[\"name\"]" | cut -s -f 4 -d\" > RUNNING_TEST
+cat namepooltest.json | ~/rtools/bin/JSON.sh | grep -F -e "[\"name\"]" | cut -s -f 4 -d\" > RUNNING_TEST
 export RUNNING_TEST=$(cat RUNNING_TEST)
 echo "Going to run $RUNNING_TEST"
