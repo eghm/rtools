893ff67f440dbd32f38e5eb1fc333eb96d1b2c4e
Erik G. H. Meade <github-m@eghm.net>
2012-10-12 12:51:52 -0700

    add Sauce Labs patch to patches which are automatically applied at version creation

--- bin/rPatches.sh
+++ bin/rPatches.sh
@@ -1,5 +1,6 @@
 patch -p1 <../rtools/etc/impex-no-user-percent.patch
 patch -p0 <../rtools/etc/KULRICE-6785.patch 
 patch -p0 <../rtools/etc/CXFWSS4JInInterceptor.patch 
+patch -p0 <../rtools/etc/SauceLabs.patch 
 #patch -p0 <../rtools/etc/IncidentReportForm.patch 
 #patch -p0 <../rtools/etc/impl-testdoc-xml.patch
