d72643e16bf1675ebdf9ac003eda7e6ab4c17564
Erik G. H. Meade <github-m@eghm.net>
2012-10-12 18:10:20 -0700

    Script to write out LegacyIT smoke tests and users

--- bin/smokeTestList.sh
+++ bin/smokeTestList.sh
@@ -1,3 +1,4 @@
+rm LegacyITsUsers.txt
 find sampleapp/ -name "*LegacyIT.java" > LegacyITs.out
 rev LegacyITs.out > LegacyITs.rev
 cut -d / -f 1 LegacyITs.rev > LegacyITs.cut1.rev
@@ -5,6 +6,14 @@ cut -d . -f 2 LegacyITs.cut1.rev > LegacyITs.cut2.rev
 rm LegacyITs.out
 rm LegacyITs.rev
 rm LegacyITs.cut1.rev
-rm LegacyITs.cut2.rev
 rev LegacyITs.cut2.rev > LegacyITs.cut2.txt
+rm LegacyITs.cut2.rev
 mv LegacyITs.cut2.txt LegacyITs.txt
+let "i=0";
+for line in $(cat LegacyITs.txt);
+do
+	echo "$line loadtester$i" >> LegacyITsUsers.txt;
+    let "i+=1";
+done; 
+rm LegacyITs.txt
+cat LegacyITsUsers.txt
