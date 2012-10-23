a9c9772d008f6103d852cedef0e1474622e4a702
Erik G. H. Meade <github-m@eghm.net>
2012-10-23 11:22:34 -0700

    remove duplicate jvm args append dont overwrite the second sed for DTS log files

--- bin/rDtsLogFiles.sh
+++ bin/rDtsLogFiles.sh
@@ -1,10 +1,11 @@
 echo "updating log4j.properties"
 echo "s|/r/logs/logs.logs|$R_HOME/logs/$1/logs.logs|g" > .rdev/$1-log4j.sed
-echo "s|R_HOME|$R_HOME|g" > .rdev/$1-log4j.sed
+echo "s|R_HOME|$R_HOME|g" >> .rdev/$1-log4j.sed
 
 sed -f .rdev/$1-log4j.sed ../rtools/etc/log4j.properties  > sampleapp/src/main/resources/log4j.properties
 sed -f .rdev/$1-log4j.sed ../rtools/etc/log4j.properties  > standalone/src/main/resources/log4j.properties
 sed -f .rdev/$1-log4j.sed ../rtools/etc/log4j.properties  > $1-log4j.properties
+cp $1-log4j.properties it/internal-tools/src/main/resources/rice-testharness-default-log4j.properties
 
 mkdir -p core/impl/src/main/java/com/veerasundar/dynamiclogger
 cp ../rtools/etc/NewLogFileForEachRunAppender.java core/impl/src/main/java/com/veerasundar/dynamiclogger/
