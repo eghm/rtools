931a0c5893517162cbe2c6e4721253523d2bfa69
Erik G. H. Meade <github-m@eghm.net>
2012-10-26 21:59:31 -0700

    script to split and push LegacyITsUsers.txt

---
+++ bin/smokeTestListPush.sh
@@ -0,0 +1,15 @@
+export SERVER_NUM=$(wc -l $1 | cut -c 1-9)
+export TEST_NUM=$(wc -l $2 | cut -c 1-9)
+export SPLIT_LINES=$((($TEST_NUM/$SERVER_NUM) + $SERVER_NUM))
+split -l $SPLIT_LINES $2 push-
+cp $1 $1.tmp
+for f in push-*;
+do
+	export SERVER=$(tail -n 1 $1.tmp)	
+	sed '$d' $1.tmp > $1.tmp2
+	mv $1.tmp2 $1.tmp
+	echo "scp $f ec2-user@$SERVER:LegacyITsUsers.txt"
+	scp $f ec2-user@$SERVER:LegacyITsUsers.txt
+done;
+rm push-*
+rm $1.tmp
