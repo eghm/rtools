aa71f1c033be92cda4ffe6f818e38c4ff60186e6
Erik G. H. Meade <github-m@eghm.net>
2012-10-30 21:30:32 -0700

    fix split line number

--- bin/smokeTestListPush.sh
+++ bin/smokeTestListPush.sh
@@ -1,6 +1,7 @@
 export SERVER_NUM=$(wc -l $1 | cut -c 1-9)
 export TEST_NUM=$(wc -l $2 | cut -c 1-9)
-export SPLIT_LINES=$((($TEST_NUM/$SERVER_NUM) + $SERVER_NUM))
+export SPLIT_LINES=$(($TEST_NUM/$SERVER_NUM))
+echo "$SERVER_NUM servers, $TEST_NUM tests, splitting into $SPLIT_LINES"
 split -l $SPLIT_LINES $2 push-
 cp $1 $1.tmp
 for f in push-*;
@@ -8,8 +9,8 @@ do
 	export SERVER=$(tail -n 1 $1.tmp)	
 	sed '$d' $1.tmp > $1.tmp2
 	mv $1.tmp2 $1.tmp
-	echo "scp -v $f ec2-user@$SERVER:LegacyITsUsers.txt"
-	scp -v $f ec2-user@$SERVER:LegacyITsUsers.txt
+	echo "scp $f ec2-user@$SERVER:LegacyITsUsers.txt"
+	scp $f ec2-user@$SERVER:LegacyITsUsers.txt
 done;
 rm push-*
 rm $1.tmp
