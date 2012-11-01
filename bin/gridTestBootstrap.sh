ac18f0df90bbbb95f70c4afde0a230715fd59fec
Erik G. H. Meade <github-m@eghm.net>
2012-11-01 14:31:04 -0700

    deprecate gridTestBootstrap.sh, just call mvnSmokeTest.sh from parallel with the same parameters.

--- bin/gridTestBootstrap.sh
+++ bin/gridTestBootstrap.sh
@@ -1,4 +1,6 @@
 # Do nothing in the bootstrap but update git and launch gridTest.sh this lets changes in gridTest.sh to be picked-up
+echo "Deprecated, just call mvnSmokeTest.sh from parallel with the same parameters."
+exit
 cd ~/rtools/
 git pull
 cd ~/$9
