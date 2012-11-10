7e92075b19dd9bf02392c6543426a824271e79e3
Erik G. H. Meade <github-m@eghm.net>
2012-11-10 14:33:26 -0800

    pass params to rSqlRest.sh

--- bin/rGitSqlRest.sh
+++ bin/rGitSqlRest.sh
@@ -1,6 +1,6 @@
 export R_HOME=~
 mkdir -p $R_HOME/rtools/bin
-mkdir $R_HOME/rtools/etc
+mkdir -p $R_HOME/rtools/etc
 cd $R_HOME/rtools/bin
 rm -f rSqlRest.sh
 wget https://raw.github.com/eghm/rtools/master/bin/rSqlRest.sh
@@ -12,7 +12,7 @@ cd $R_HOME/rtools/etc
 rm -f sqlrestconf.xml
 wget https://raw.github.com/eghm/rtools/master/etc/sqlrestconf.xml
 cd $R_HOME
-$R_HOME/rtools/bin/rSqlRest.sh 
+$R_HOME/rtools/bin/rSqlRest.sh $*
 
 
 
