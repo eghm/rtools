b2d9b31c272d04c4736e9b240e56231bccbbb6d5
Erik G. H. Meade <github-m@eghm.net>
2012-11-10 14:02:15 -0800

    chmod the scripts executable

--- bin/rGitSqlRest.sh
+++ bin/rGitSqlRest.sh
@@ -3,7 +3,9 @@ mkdir -p $R_HOME/rtools/bin
 mkdir $R_HOME/rtools/etc
 cd $R_HOME/rtools/bin
 wget https://raw.github.com/eghm/rtools/master/bin/rSqlRest.sh
+chmod 755 rSqlRest.sh
 wget https://raw.github.com/eghm/rtools/master/bin/rSqlRestInstall.sh
+chmod 755 rSqlRestInstall.sh
 cd $R_HOME/rtools/etc
 wget https://raw.github.com/eghm/rtools/master/etc/sqlrestconf.xml
 cd $R_HOME
