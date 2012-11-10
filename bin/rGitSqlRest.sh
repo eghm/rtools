a1af2c3a485dbc53afd3a383f98b037873d7afa7
Erik G. H. Meade <github-m@eghm.net>
2012-11-10 13:57:56 -0800

    script to install sqlrest from rtools git

---
+++ bin/rGitSqlRest.sh
@@ -0,0 +1,13 @@
+export R_HOME=.
+mkdir -p $R_HOME/rtools/bin
+mkdir $R_HOME/rtools/etc
+cd $R_HOME/rtools/bin
+wget https://raw.github.com/eghm/rtools/master/bin/rSqlRest.sh
+wget https://raw.github.com/eghm/rtools/master/bin/rSqlRestInstall.sh
+cd $R_HOME/rtools/etc
+wget https://raw.github.com/eghm/rtools/master/etc/sqlrestconf.xml
+cd $R_HOME
+rSqlRest.sh 
+
+
+
