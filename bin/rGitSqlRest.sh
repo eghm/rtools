5785a3338de0b067fb006b2f98468fc16b553679
Erik G. H. Meade <github-m@eghm.net>
2012-11-10 14:23:29 -0800

    rm files before wgetting them

--- bin/rGitSqlRest.sh
+++ bin/rGitSqlRest.sh
@@ -2,11 +2,14 @@ export R_HOME=~
 mkdir -p $R_HOME/rtools/bin
 mkdir $R_HOME/rtools/etc
 cd $R_HOME/rtools/bin
+rm rSqlRest.sh
 wget https://raw.github.com/eghm/rtools/master/bin/rSqlRest.sh
 chmod 755 rSqlRest.sh
+rm rSqlRestInstall.sh
 wget https://raw.github.com/eghm/rtools/master/bin/rSqlRestInstall.sh
 chmod 755 rSqlRestInstall.sh
 cd $R_HOME/rtools/etc
+rm sqlrestconf.xml
 wget https://raw.github.com/eghm/rtools/master/etc/sqlrestconf.xml
 cd $R_HOME
 $R_HOME/rtools/bin/rSqlRest.sh 
