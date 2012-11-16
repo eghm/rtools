39b50a4c5609a27e1999b8dc8607c1ab0c05c4c4
Erik G. H. Meade <github-m@eghm.net>
2012-11-15 23:24:21 -0800

    java home and path

--- bin/remoteMvnTestCompile.sh
+++ bin/remoteMvnTestCompile.sh
@@ -1,4 +1,5 @@
+export JAVA_HOME=~/jdk1.7.0_10
 export M2_HOME=~/apache-maven-3.0.4/
-export PATH=$PATH:$M2_HOME/bin:~/rtools/bin
+export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin:~/rtools/bin
 cd $1
 mvn test-compile -f sampleapp/pom.xml
