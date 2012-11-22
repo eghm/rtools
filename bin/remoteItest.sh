7319521d418d06d7cb6ae5b2c8fe006a2453675e
Erik G. H. Meade <github-m@eghm.net>
2012-11-22 14:19:11 -0800

    remoteItest.sh

--- bin/remoteItest.sh
+++ bin/remoteItest.sh
@@ -22,6 +22,6 @@ do
     ln -s ../logs/$9/$logname.test-compile.out $logname.test-compile.out
     mvn -version  >> $logname.test-compile.out
 
-    echo "mvn -Pitests verify -Ddts.log.filename=itests-log.log -Dit.test=$TEST $*" >> $logname.out
-    mvn -Pitests verify -Ddts.log.filename=itests-log.log -Dit.test=$TEST $* >> $logname.out
+    echo "mvn -Pitests verify -Ddts.log.filename=itests-log.log -Dit.test=$TEST $2 $3 $4 $5 $6 $7 $8 $9" >> $logname.out
+    mvn -Pitests verify -Ddts.log.filename=itests-log.log -Dit.test=$TEST $2 $3 $4 $5 $6 $7 $8 $9 >> $logname.out
 done
