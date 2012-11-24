3ab4f814426b037233eb8f1cbbb4697bbffaf5e9
Erik G. H. Meade <github-m@eghm.net>
2012-11-24 12:26:57 -0800

    cu xml patch, db 2 imped sed update

--- bin/rPatches.sh
+++ bin/rPatches.sh
@@ -1,3 +1,12 @@
+echo -e "\nApplying rtools/etc/cu-xml.patch"
+mkdir -p core/impl/src/main/java/org/kuali/rice/core/impl/xml
+mkdir -p kim/kim-impl/src/main/java/org/kuali/rice/kim/impl/xml
+mkdir -p kim/kim-impl/src/main/java/org/kuali/rice/kim/impl/data/xml
+mkdir -p kim/kim-impl/src/main/java/org/kuali/rice/kim/impl/permission/xml
+mkdir -p kim/kim-impl/src/main/java/org/kuali/rice/kim/impl/role/xml
+mkdir -p kim/kim-impl/src/main/java/org/kuali/rice/kim/impl/user/xml
+patch -p0 <../rtools/etc/cu-xml.patch
+
 # the impex patches are actually applied in rImpexPrep.sh, but since that directory gets deleted, they are also applied here.
 echo -e "\nImpex pateched to remove % in user table bys"
 patch -p1 <../rtools/etc/impex-no-user-percent.patch
