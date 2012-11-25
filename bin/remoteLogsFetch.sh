0d28e2cb5a6e2944aa1d01b843e0b06732cc896d
Erik G. H. Meade <github-m@eghm.net>
2012-11-24 21:04:27 -0800

    remote itest updates

--- bin/remoteLogsFetch.sh
+++ bin/remoteLogsFetch.sh
@@ -1,5 +1,6 @@
+export DTS=$(date +%Y%m%d%H%M)
 for line in $(cat servers.txt);
 do
-    mkdir -p $1/$line
-	scp $line:logs/$1/*.* $1/$line/
+    mkdir -p $1/$DTS/$line
+	scp $line:logs/$1/*.* $1/$DTS/$line/
 done;
