cc348ed90dbedb5887771239a4b11b23bf29f1b8
Erik G. H. Meade <github-m@eghm.net>
2012-11-25 18:31:24 -0800

    csv vs txt updates

--- bin/smokeTestList.sh
+++ bin/smokeTestList.sh
@@ -3,15 +3,15 @@
 #USE LegacyITsUsers.txt with expanded list of saucelabs ids
 rm saucelabs.looped
 
-export SAUCE_NUM=$(wc -l saucelabs.txt.expanded | cut -c 1-9)
+export SAUCE_NUM=$(wc -l saucelabs.csv.expanded | cut -c 1-9)
 export TEST_NUM=$(wc -l LegacyITsUsers.txt | cut -c 1-9)
 export SAUCE_LOOPS=$(($TEST_NUM/$SAUCE_NUM))
 export SAUCE_MOD=$(expr $TEST_NUM % $SAUCE_NUM)
 
 while [  $SAUCE_LOOPS -gt 0 ]; do
-    cat  saucelabs.txt.expanded >> saucelabs.looped;
+    cat  saucelabs.csv.expanded >> saucelabs.looped;
 	let SAUCE_LOOPS-=1
 done
-head -n $SAUCE_MOD saucelabs.txt.expanded >> saucelabs.looped
+head -n $SAUCE_MOD saucelabs.csv.expanded >> saucelabs.looped
 
 paste -d , LegacyITsUsers.txt saucelabs.looped
