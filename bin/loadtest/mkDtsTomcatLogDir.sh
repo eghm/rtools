5d6fbbd8f8a4ff51e4bfb396bfddc9e256d629fa
Erik G. H. Meade <github-m@eghm.net>
2013-01-13 13:26:05 -0800

    sleep 60 for tomcat to shutdown before moving any existing logs into tomcat/logs/bak

--- bin/loadtest/mkDtsTomcatLogDir.sh
+++ bin/loadtest/mkDtsTomcatLogDir.sh
@@ -3,6 +3,7 @@ export DTS=$(date +%Y-%m-%d/%H-%M)
 export T_LOGS=/usr/local/tomcat/logs
 export DTS_LOGS=$T_LOGS/$DTS
 mkdir -p $DTS_LOGS
+sleep 60
 cd $T_LOGS ; mkdir -p bak ; mv *.out bak/ ; mv *.log bak/ ; mv *.txt bak/
 cd
 /usr/local/tomcat/bin/startup.sh
