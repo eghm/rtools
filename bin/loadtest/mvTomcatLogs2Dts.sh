9e5f5568aa00546fa095d7f0127bda1bb6921b50
Erik G. H. Meade <github-m@eghm.net>
2013-01-14 12:31:17 -0800

    rest exports as we might have logged out, read the DTS from ~/dts.txt

--- bin/loadtest/mvTomcatLogs2Dts.sh
+++ bin/loadtest/mvTomcatLogs2Dts.sh
@@ -1,3 +1,7 @@
+export DTS=$(cat ~/dts.txt)
+export T_LOGS=/usr/local/tomcat/logs
+export DTS_LOGS=$T_LOGS/$DTS
+
 mv $T_LOGS/catalina.out $DTS_LOGS/ ; mv $T_LOGS/*.log $DTS_LOGS/ ; mv $T_LOGS/localhost*.txt $DTS_LOGS/ ; mv $T_LOGS/*.hprof $DTS_LOGS/
 # start tomcat backup for wget of logs
 /usr/local/tomcat/bin/startup.sh
