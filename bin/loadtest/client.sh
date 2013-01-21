a1f737d984b5e984f526cbf65f201c301ede18b0
Erik G. H. Meade <github-m@eghm.net>
2013-01-21 14:21:26 -0800

    draft commands using JMeterPluginsCMD to generate graphs from jtl result files

--- bin/loadtest/client.sh
+++ bin/loadtest/client.sh
@@ -57,6 +57,23 @@ sipsTiff2Png.sh $(pwd)
 mkdir -p tiffs/$DTS
 mv *.tiff tiffs/$DTS/
 
+# http://code.google.com/p/jmeter-plugins/wiki/JMeterPluginsCMD
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesOverTime.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ThreadsStateOverTime.png --input-jtl ResultsTable.jtl --plugin-type ThreadsStateOverTime --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png BytesThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type BytesThroughputOverTime --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png HitsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type HitsPerSecond --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png LatenciesOverTime.png --input-jtl ResultsTable.jtl --plugin-type LatenciesOverTime --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png PerfMon.png --input-jtl ResultsTable.jtl --plugin-type PerfMon --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseCodesPerSecond.png --input-jtl ResultsTable.jtl --plugin-type ResponseCodesPerSecond --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesDistribution.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesDistribution --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesPercentiles.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesPercentiles --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type ThroughputOverTime --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png ThroughputVsThreads.png --input-jtl ResultsTable.jtl --plugin-type ThroughputVsThreads --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png TimesVsThreads.png --input-jtl ResultsTable.jtl --plugin-type TimesVsThreads --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png TransactionsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type TransactionsPerSecond --width 800 --height 600
+/java/jmeter-2.8/lib/ext/JMeterPluginsCMD.sh --generate-png PageDataExtractorOverTime.png --input-jtl ResultsTable.jtl --plugin-type PageDataExtractorOverTime --width 800 --height 600
+
+
 mv *.jtl $DTS/
 mv *.png $DTS/
 mv *.log $DTS/
