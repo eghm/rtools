cba010cd4f462f8c746f59da9fcb071afec6c5e7
Erik G. H. Meade <github-m@eghm.net>
2013-01-21 21:16:41 -0800

    wait 2 minutes for the app to start, then do the DTS specific stuff

--- bin/loadtest/mkDtsTomcatLogDir.sh
+++ bin/loadtest/mkDtsTomcatLogDir.sh
@@ -8,12 +8,13 @@ then
     exit
 fi
 
-export DTS=$(date +%Y-%m-%d/%H-%M)
-export DTS_LOGS=$T_LOGS/$DTS
-mkdir -p $DTS_LOGS
 cd $T_LOGS ; mkdir -p bak ; mv *.out bak/ ; mv *.log bak/ ; mv *.txt bak/
 cd
 $CATALINA_HOME/bin/startup.sh
+sleep 120
+export DTS=$(date +%Y-%m-%d/%H-%M)
+export DTS_LOGS=$T_LOGS/$DTS
+mkdir -p $DTS_LOGS
 echo "DTS $DTS"
 echo "$DTS" > ~/dts.txt
 
