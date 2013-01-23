0a29c0d9a38222225ccd2a1a338bc344866a71c7
Erik G. H. Meade <github-m@eghm.net>
2013-01-22 17:22:10 -0800

    delete temp file

--- bin/loadtest/jmeterThreadsRamp.sh
+++ bin/loadtest/jmeterThreadsRamp.sh
@@ -1,3 +1,4 @@
 xml ed -u "//stringProp[@name='ThreadGroup.num_threads']" -v $2 $1.jmx > $1$2in.jmx
 xml ed -u "//stringProp[@name='ThreadGroup.ramp_time']" -v $3 $1$2in.jmx > $1$2in$3.jmx
+rm $1$2in.jmx
 mv $1$2in$3.jmx $1.jmx
