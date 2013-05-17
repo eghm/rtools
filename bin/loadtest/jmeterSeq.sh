91415aac267a63c7eeec53a45cd47fcaa58cf605
Erik G. H. Meade <github-m@eghm.net>
2013-05-16 22:25:53 -0700

    parallel arguements

--- bin/loadtest/jmeterSeq.sh
+++ bin/loadtest/jmeterSeq.sh
@@ -5,12 +5,11 @@ then
 	exit
 fi
 
-parallel --tag --nonall --sshloginfile ~/servers.txt rtools/bin/loadtest/mkDtsTomcatLogDir.sh $5
+parallel --tag --nonall --sshloginfile ~/servers.txt rtools/bin/loadtest/mkDtsTomcatLogDir.sh < $5
 scp tomcat@env11.rice.kuali.org:dts.txt .
 /java/jmeter-2.8/bin/jmeter.sh -n -t $2.jmx
 sleep 120
 parallel --tag --nonall --sshloginfile ~/servers.txt rtools/bin/loadtest/mvTomcatLogs2Dts.sh
-cp ../jvm.txt .
 /r/rtools/bin/loadtest/client.sh $1 $3 $4
 
 
