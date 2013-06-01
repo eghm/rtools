d36f9c68ca8b5b364f4091a8f7c4dddd166a4d19
Erik G. H. Meade <github-m@eghm.net>
2013-06-01 14:37:32 -0700

    scripts to work with JMeter parameters vs using xmlstar

---
+++ bin/loadtest/jmeterSeq2.sh
@@ -0,0 +1,16 @@
+# $1 server, $2 jmeter test name without the .jmx $3 number of threads $4 number of loops $5 ramp seconds $6 wiki username $7 wiki passtoken $8 java agent line to be appended to CATALINA_OPTS
+if [ ! -e "$2".jmx ] 
+then
+	echo "$2.jmx doesn't exist, exiting."
+	exit
+fi
+
+parallel --tag --nonall --sshloginfile ~/servers.txt rtools/bin/loadtest/mkDtsTomcatLogDir.sh $8
+scp tomcat@$1:dts.txt .
+wget http://$1/tomcat/logs/env.jsp -O env.html
+$JMETER_HOME/bin/jmeter.sh -n -t $2.jmx -Jthreads=$3 -Jloops=$4 -Jrampseconds=$5
+sleep 120
+parallel --tag --nonall --sshloginfile ~/servers.txt rtools/bin/loadtest/mvTomcatLogs2Dts.sh
+$R_HOME/rtools/bin/loadtest/client.sh $1 $6 $7
+
+
