ad1ce894ab0899f1e849a01ad5247aaff73fa454
Erik G. H. Meade <github-m@eghm.net>
2013-06-01 21:17:48 -0700

    fix paths

--- bin/loadtest/client2.sh
+++ bin/loadtest/client2.sh
@@ -83,7 +83,7 @@ $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThreadsStateOverTime.png
 $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png BytesThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type BytesThroughputOverTime --width 800 --height 600
 $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png HitsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type HitsPerSecond --width 800 --height 600
 $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png LatenciesOverTime.png --input-jtl ResultsTable.jtl --plugin-type LatenciesOverTime --width 800 --height 600
-$JMETER_HOME/ext/JMeterPluginsCMD.sh --generate-png PerfMon.png --input-jtl ResultsTable.jtl --plugin-type PerfMon --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png PerfMon.png --input-jtl ResultsTable.jtl --plugin-type PerfMon --width 800 --height 600
 $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseCodesPerSecond.png --input-jtl ResultsTable.jtl --plugin-type ResponseCodesPerSecond --width 800 --height 600
 $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesDistribution.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesDistribution --width 800 --height 600
 $JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesPercentiles.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesPercentiles --width 800 --height 600
