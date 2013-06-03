4a7bc7f98b362bcabca1c56cb121b6b246066f28
Erik G. H. Meade <github-m@eghm.net>
2013-06-02 20:18:45 -0700

    extract jmeter graphing and remove user and password variables which aren't being used.

--- bin/loadtest/client2.sh
+++ bin/loadtest/client2.sh
@@ -1,20 +1,11 @@
-# $1 SERVER $2 USER $3 PASS $4 JM_NUM $5 JM_LOOP $6 JM_RAMP
+# $1 SERVER $2 JM_NUM $3 JM_LOOP $4 JM_RAMP
 # RUN JMETER and have test and outputs in the directory this is run from
 if [ -z "$1" ]
 then
     echo "Required parameter, server"
     exit;
 fi
-if [ -z "$2" ]
-then
-    echo "Required parameter, user"
-    exit;
-fi
-if [ -z "$3" ]
-then
-    echo "Required parameter, password"
-    exit;
-fi
+export SERVER="$1"
 
 if [ -z "$R_HOME" ]
 then
@@ -22,14 +13,26 @@ then
     exit;
 fi
 
-if [ -z "$JMETER_HOME" ]
+if [ ! -e testparams.txt]
 then
-    echo "Required env var JMETER_HOME not set."
-    exit;
-fi
+    if [ -z "$4" ]
+    then
+        echo "Required parameter, threads (users) missing"
+        exit;
+    fi
+    if [ -z "$5" ]
+    then
+        echo "Required parameter, loops missing"
+        exit;
+    fi
+    if [ -z "$6" ]
+    then
+        echo "Required parameter, rampseconds missing"
+        exit;
+    fi
 
-export SERVER="$1"
-echo "$4 users x $5 ramped up in $6 seconds" > testparams.txt
+    echo "$2 users x $3 ramped up in $4 seconds" > testparams.txt
+fi
 
 # get the release and build from the given server
 wget http://$SERVER/portal.do -O portal.html
@@ -52,6 +55,12 @@ else
     echo "2.3.0" > release.txt
 fi
 
+if [ !- e portal.html ]
+then
+    echo "portal.html does not exist!  Tomcat probably down or Dumping Memory!  Exiting"
+    exit
+fi
+
 export R_RELEASE=$(cat release.txt)
 echo $R_RELEASE
 
@@ -71,28 +80,13 @@ fi
 wget -r --no-parent -nH --cut-dirs=2 -R index.html http://$SERVER/tomcat/logs/$DTS/
 if [ ! -d "$DTS" ]
 then
-    echo "$DTS directory does not exist!  Tomcat probably down!  Exiting"
+    echo "$DTS directory does not exist!  Tomcat probably down or Dumping Memory!  Exiting"
     exit;
 fi
 
 echo "TODO parse the JAVA_OPTS and CATALINA_OPTS out of env.html (attached) and put into jvm.txt for inclusion in the wiki page." >> jvm.txt
 
-# http://code.google.com/p/jmeter-plugins/wiki/JMeterPluginsCMD
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesOverTime.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThreadsStateOverTime.png --input-jtl ResultsTable.jtl --plugin-type ThreadsStateOverTime --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png BytesThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type BytesThroughputOverTime --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png HitsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type HitsPerSecond --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png LatenciesOverTime.png --input-jtl ResultsTable.jtl --plugin-type LatenciesOverTime --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png PerfMon.png --input-jtl ResultsTable.jtl --plugin-type PerfMon --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseCodesPerSecond.png --input-jtl ResultsTable.jtl --plugin-type ResponseCodesPerSecond --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesDistribution.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesDistribution --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesPercentiles.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesPercentiles --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type ThroughputOverTime --width 800 --height 600
-#$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThroughputVsThreads.png --input-jtl ResultsTable.jtl --plugin-type ThroughputVsThreads --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png TimesVsThreads.png --input-jtl ResultsTable.jtl --plugin-type TimesVsThreads --width 800 --height 600
-$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png TransactionsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type TransactionsPerSecond --width 800 --height 600
-#/$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png PageDataExtractorOverTime.png --input-jtl ResultsTable.jtl --plugin-type PageDataExtractorOverTime --width 800 --height 600
-
+$R_HOME/bin/loadtesting/jmeterGraphs.sh
 
 mv *.jtl $DTS/
 mv *.pdf $DTS/
---
+++ bin/loadtest/jmeterGraphs.sh
@@ -0,0 +1,21 @@
+if [ -z "$JMETER_HOME" ]
+then
+    echo "Required env var JMETER_HOME not set."
+    exit;
+fi
+
+# http://code.google.com/p/jmeter-plugins/wiki/JMeterPluginsCMD
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesOverTime.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesOverTime --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThreadsStateOverTime.png --input-jtl ResultsTable.jtl --plugin-type ThreadsStateOverTime --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png BytesThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type BytesThroughputOverTime --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png HitsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type HitsPerSecond --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png LatenciesOverTime.png --input-jtl ResultsTable.jtl --plugin-type LatenciesOverTime --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png PerfMon.png --input-jtl ResultsTable.jtl --plugin-type PerfMon --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseCodesPerSecond.png --input-jtl ResultsTable.jtl --plugin-type ResponseCodesPerSecond --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesDistribution.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesDistribution --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ResponseTimesPercentiles.png --input-jtl ResultsTable.jtl --plugin-type ResponseTimesPercentiles --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThroughputOverTime.png --input-jtl ResultsTable.jtl --plugin-type ThroughputOverTime --width 800 --height 600
+#$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png ThroughputVsThreads.png --input-jtl ResultsTable.jtl --plugin-type ThroughputVsThreads --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png TimesVsThreads.png --input-jtl ResultsTable.jtl --plugin-type TimesVsThreads --width 800 --height 600
+$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png TransactionsPerSecond.png --input-jtl ResultsTable.jtl --plugin-type TransactionsPerSecond --width 800 --height 600
+#/$JMETER_HOME/lib/ext/JMeterPluginsCMD.sh --generate-png PageDataExtractorOverTime.png --input-jtl ResultsTable.jtl --plugin-type PageDataExtractorOverTime --width 800 --height 600
