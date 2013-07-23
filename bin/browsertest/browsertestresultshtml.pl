8ccbf1dcc021236c9c92ef048839937661d9ea7f
Erik G. H. Meade <github-m@eghm.net>
2013-07-22 20:22:03 -0700

    ugly draft for html browser test results

---
+++ bin/browsertest/browsertestresultshtml.pl
@@ -0,0 +1,136 @@
+#!/usr/local/wu/bin/perl
+# From the directory with all the smoke test logs:
+# perl browsertestresults.pl -f=$(pwd) > results.txt
+use Getopt::Long;
+use Tie::File;
+use Date::Manip
+GetOptions( "f=s" => \$importdir); # -f=/path/
+opendir(DIR, $importdir);
+
+my @filelist;
+foreach my $file (readdir(DIR)) {
+    if ($file =~ m|.*\.out$|s) {
+        #print "file: $file\n";
+        push(@filelist, $file);
+    }
+}
+
+closedir(DIR);
+
+my @tests;
+foreach my $importfile (@filelist) {
+    #print "processing $importfile\n";
+    `rm $importfile.results`;
+    
+    #print "grep \" sessionId is \" $importfile";
+    my @testSessionIds = `grep " sessionId is " $importfile`;
+    foreach my $testline (@testSessionIds) {
+        #print "\t$testline\n";
+
+        ($testline =~ m|^(\S*)\s.*|s);
+        my $test = $1;
+        #print "\t$test\n";
+        if ("$test" ~~ @tests) {
+        } else {
+            push(@tests, $test);
+        }
+
+        ($testline =~ m|^.*\s(.*)\s$|s);
+        my $sessionId = $1;
+        #print "\t$test $sessionId\n";
+
+        my @sessionIdStates = `grep $sessionId $importfile`;
+        foreach my $sessionState (@sessionIdStates) {
+            if ($sessionState =~ m|^Registering session.*|s) {
+                #print "\t\t$sessionState\n";
+                ($sessionState =~ m|^Registering session (\S*) $sessionId|s);
+                #print "\t\t\t$1\n";
+                my $state = $1;
+                `echo $test $state >> $importfile.results`;
+            }
+        }
+    }
+}
+
+@tests = sort(@tests);
+my $testLengthMax = 0;
+foreach my $test (@tests) {
+    if (length($test) gt $testLengthMax) {
+        $testLengthMax = length($test);
+    }
+    #print "$test\n";
+}
+
+
+# confluence table header
+#my $header = "|| Test / ENV ||";
+#foreach my $testenv (@filelist) {
+#    if ($testenv =~ m|^\S*?\-(.*)\-\d*.*|s) {
+#      $header .= $1 . "||";
+#    }
+#}
+
+my $header = "<html><head></head><body><table>";
+my $headerspace = "    ";;
+for ($i = $testLengthMax; $i >= 0; $i--) {
+    $headerspace .= " ";
+}
+
+my @testenvs;
+my $testenvsLengthMax = 0;
+foreach my $testenv (@filelist) {
+    if ($testenv =~ m|^\S*?\-(.*)\-\d*.*|s) {
+        my $envtext = $1;
+        (my $envreadable = $envtext) =~ s/_/ /g;
+        (my $envreadable2 = $envreadable) =~ s/-/ /g;
+        push(@testenvs, $envreadable2);
+        if (length($envtext) > $testenvsLengthMax) {
+            $testenvsLengthMax = length($envtext);
+        }
+    }
+}
+
+# aligned along the top
+for ($i = 0; $i < $testenvsLengthMax; $i++) {
+    $header .= "<tr><td>" . $headerspace . "</td>";
+    foreach my $testenv (@testenvs) {
+        if (length($testenv) > $i) {
+            $header .= "<td>" . substr($testenv, $i, 1) . "</td>";
+        } else {
+            $header .= "<td>  </td>";
+        }
+    }
+    $header .= "</tr>\n";
+}
+
+
+print "$header\n";
+foreach my $test (@tests) {
+    my $testsresult = "<td>" . $test;
+    for ($i = $testLengthMax - length($test); $i >= 0; $i--) {
+        $testsresult .= " ";
+    }
+    $testsresult .=  "</td>";
+    foreach my $testenv (@filelist) {
+        my $result = `grep $test $testenv.results`;
+
+    ($testenv =~ m|.*-(.*-.*-.*-.*)?-\d*|s);
+    my $dirMatch = $1;
+#	print "\t$dirMatch\n";
+#	`ls -d *SmokeTest*$dirMatch* > $dirMatch.txt`;
+
+        my $testDir = `ls -d $test-$dirMatch*`;
+        $testDir = substr($testDir, 0, length($testDir) - 1);
+#        print "\t\t$testDir\n";
+        if ($result =~ m|^.* passed$|s) {
+            $testsresult .= "<td><a href=\"$testDir/$testDir-video.flv\">S</a></td>";
+        } elsif ($result =~ m|^.* failed$|s)  {
+            $testsresult .= "<td><a href=\"$testDir/$testDir-video.flv\">F</a></td>";
+        } else {
+            $testsresult .= "<td>-</td>";
+        }
+    } 
+    print "<tr>$testsresult</tr>\n";
+}
+print "</table></body></html>"
+
