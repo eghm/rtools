a9ebb632fc3f01ef027df51a61ed94297033a4a8
Erik G. H. Meade <github-m@eghm.net>
2013-11-03 18:15:23 -0800

    remove all to updating 2.4 db with scripts

--- bin/mysqlCreateDBs.sh
+++ bin/mysqlCreateDBs.sh
@@ -6,22 +6,22 @@ echo -e "\nCreating mysql DBs $1test, $1wip, $1clean"
 mysqlCreateDB.sh $1test root $2
 mysql -u root -p$2 $1test < scripts/ddl/mysql-rice-test-tables.sql
 mvn-impex.sh $1test $2 $3 $4
-mysqlUpdate24.sh $1test $3 $4
+#mysqlUpdate24.sh $1test $3 $4
 rSquirrel.sh $1 test $3 $4
 
 mysqlCreateDB.sh $1wip root $2
 mvn-impex.sh $1wip $2 $3 $4
-mysqlUpdate24.sh $1wip $3 $4
+#mysqlUpdate24.sh $1wip $3 $4
 rSquirrel.sh $1 wip $3 $4
 
 mysqlCreateDB.sh $1clean root $2
 mvn-impex.sh $1clean $2 $3 $4
-mysqlUpdate24.sh $1clean $3 $4
+#mysqlUpdate24.sh $1clean $3 $4
 rSquirrel.sh $1 clean $3 $4
 
-mysqlCreateDB.sh $1unpatched root $2
-mvn-impex.sh $1unpatched $2 $3 $4
-rSquirrel.sh $1 unpatched $3 $4
+#mysqlCreateDB.sh $1unpatched root $2
+#mvn-impex.sh $1unpatched $2 $3 $4
+#rSquirrel.sh $1 unpatched $3 $4
 
 #rSqlRest.sh $1wip $3 $4
 
