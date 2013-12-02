c7380e5c682107d39350f05620497b47533480d9
Erik G. H. Meade <github-m@eghm.net>
2013-12-02 13:45:56 -0800

    reload erroring on rice-middleware

--- bin/rKradreload.sh
+++ bin/rKradreload.sh
@@ -1,4 +1,24 @@
-echo -e "\nInstalling Reloading DataDictionary, see https://wiki.kuali.org/display/KULRICE/Reloading+Data+Dictionary+Setup"
+echo -e "\nInstalling Reloading DataDictionary for rice-framework/krad-sampleapp/web, see https://wiki.kuali.org/display/KULRICE/Reloading+Data+Dictionary+Setup"
+if [ -f rice-framework/krad-sampleapp/web/pom.xml.orig ]
+then
+    echo "rice-framework/krad-sampleapp/pom.xml.orig already exists, skipping."
+else
+    echo "backing up rice-framework/krad-sampleapp/web/pom.xml to rice-framework/krad-sampleapp/web/pom.xml.orig"
+    mv rice-framework/krad-sampleapp/web/pom.xml rice-framework/krad-sampleapp/web/pom.xml.orig
+    echo "updating web/pom.xml for ReloadingDataDictionary see https://wiki.kuali.org/display/KULRICE/Reloading+Data+Dictionary+Setup"
+    sed 's|</dependencies>|  <dependency>\
+          <groupId>${project.groupId}</groupId>\
+          <artifactId>rice-development-tools</artifactId>\
+          <version>${project.version}</version>\
+        </dependency>\
+      </dependencies> |g' rice-framework/krad-sampleapp/web/pom.xml.orig > rice-framework/krad-sampleapp/web/pom.xml
+fi
+
+exit
+
+# get erros with rice-middleware
+
+echo -e "\nInstalling Reloading DataDictionary for rice-middleware/web, see https://wiki.kuali.org/display/KULRICE/Reloading+Data+Dictionary+Setup"
 if [ -f rice-middleware/web/pom.xml.orig ]
 then
     echo "rice-middleware/web/pom.xml.orig already exists, skipping."
@@ -14,7 +34,6 @@ else
       </dependencies> |g' rice-middleware/web/pom.xml.orig > rice-middleware/web/pom.xml
 fi
 
-exit
 
 # this is the old way, before it could be set via config param
 
