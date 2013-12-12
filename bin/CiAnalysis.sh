be8a61243f6bce8b1f9d210fd478ccab194c98eb
Erik G. H. Meade <github-m@eghm.net>
2013-12-12 15:40:31 -0800

    CiAnalysis.sh hard coded path fix

--- bin/CiAnalysis.sh
+++ bin/CiAnalysis.sh
@@ -1,4 +1,10 @@
-cd /r/rtools/bin
+# Directory to put results in, CAS username, CAS password
+
+#JenkinsResults.sh $2 $3 $1
+
+cd $R_HOME/rtools/bin
+
+touch ExtEmailBlank.txt 
 
 for f in $1/*.json
 do
@@ -9,7 +15,7 @@ do
     groovy CiJsonObj.groovy $f > $f.1
 
     # List test failures and errors and group tests by errors
-    groovy ExtEmailResultsDiff.groovy /Users/eghm/Desktop/201311-Desktop/IT-blank.txt $f.1 > $f.2
+    groovy ExtEmailResultsDiff.groovy ExtEmailBlank.txt $f.1 > $f.2
 
     # Identify existing (open) Jiras for failed tests flag those that don't have Jiras
     groovy ExtResultsJiras.groovy $f.2 > $f.out
@@ -24,5 +30,7 @@ do
 #    groovy CiJiraGroup.groovy $f
 
 done
+
+rm ExtEmailBlank.txt
 
 ./CiJiraGroup.sh $1
