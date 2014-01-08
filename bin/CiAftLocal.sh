df985835a08382067150c5d1b61014dc34b17463
Erik G. H. Meade <github-m@eghm.net>
2014-01-07 23:04:29 -0800

    start for running CI Aft failures locally

--- bin/CiAftLocal.sh
+++ bin/CiAftLocal.sh
@@ -3,8 +3,8 @@ find $1/JiraGroups -name '*.jira' > Afts2Run.txt
 
 for f in $(cat Afts2Run.txt) ; do 
     echo $f | rev > Aft2Run.rev
-    AFT_REV = cut -f 1 -d Aft2Run.rev     
-    AFT = $(echo $AFT_REV | rev)
+    AFT_REV=cut -f 1 -d Aft2Run.rev     
+    AFT=$(echo $AFT_REV | rev)
     echo $AFT
 done
 
