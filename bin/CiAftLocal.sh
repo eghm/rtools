d16046c66e85d46cb1e73de2af271e5f506d61cd
Erik G. H. Meade <github-m@eghm.net>
2014-01-07 23:11:55 -0800

    start for running CI Aft failures locally

--- bin/CiAftLocal.sh
+++ bin/CiAftLocal.sh
@@ -4,10 +4,12 @@ find $1/JiraGroups -name '*.jira' > Afts2Run.txt
 for f in $(cat Afts2Run.txt) ; do 
     echo $f | rev > Aft2Run.rev
     AFT_REV=$(cut -f 1 -d - Aft2Run.rev)
-    AFT=$(echo $AFT_REV | rev)
+    echo $AFT_REV | rev > Aft.jira
+    AFT=$(cut -f 1-2 -d . Aft.jira)
     ${$AFT/./#}
     echo $AFT
     rm Aft2Run.rev
+    rm Aft.jira
 
     echo "mvn failsafe:integration-test -Pstests -Dremote.jgrowl.enabled=false -Dremote.driver.highlight=false -Dremote.public.url=$2 -Dit.test=$3"
 done
