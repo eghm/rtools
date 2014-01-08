f3545e59047bec0b93d74bdb3ba837d16727a4be
Erik G. H. Meade <github-m@eghm.net>
2014-01-07 23:02:33 -0800

    start for running CI Aft failures locally

---
+++ bin/CiAftLocal.sh
@@ -0,0 +1,12 @@
+# JenkinsResults directory, remote.public.url, it.test
+find $1/JiraGroups -name '*.jira' > Afts2Run.txt
+
+for f in $(cat Afts2Run.txt) ; do 
+    echo $f | rev > Aft2Run.rev
+    AFT_REV = cut -f 1 -d Aft2Run.rev     
+    AFT = (echo $AFT_REV | rev)
+    echo $AFT
+done
+
+
+echo "mvn failsafe:integration-test -Pstests -Dremote.jgrowl.enabled=false -Dremote.driver.highlight=false -Dremote.public.url=$2 -Dit.test=$3"
