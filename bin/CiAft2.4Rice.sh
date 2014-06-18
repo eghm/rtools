f2fad81fce49d3c6e46ecdb92c4b86d468443578
Erik G. H. Meade <github-m@eghm.net>
2014-06-18 15:31:21 -0700

    draft automation of 2.4 rice AFT CI Anaylsis

---
+++ bin/CiAft2.4Rice.sh
@@ -0,0 +1,22 @@
+stime=$(date '+%s')
+export DTS=$(date +%Y%m%d%H%M)
+export RESULTS_DIR=rice-2.4-smoke-test-$DTS
+
+export AFT_DIR=$(pwd)
+
+# mvnLinks
+export rDir=${PWD##*/}
+export M2_REPO=/java/m2/$rDir
+
+cd rice-tools-test && mvn failsafe:integration-test -Pstests -Dmaven.failsafe.skip=false -Dit.test=JenkinsLastCompletedBuildNumber -Dcas.username=$1 -Dcas.password=$2 -Djenkins.jobs=$4 -Dremote.driver.dontTearDownOnFailure=y -Dmaven.repo.local=$M2_REPO > rice-2.4-smoke-test-last.txt
+
+JOBS=$(cat rice-2.4-smoke-test-last.txt)
+
+cd ..
+
+CiJenkinsResultsFor.sh $1 $2 $RESULTS_DIR $JOBS
+
+CiAnalysis.sh $RESULTS_DIR 
+
+CiAftLocalRice.sh $RESULTS_DIR $AFT_DIR
+
