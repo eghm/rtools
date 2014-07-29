e0313c6d36d525a06cd6bc0c47b584159d8a6717
Erik G. H. Meade <github-m@eghm.net>
2014-07-28 19:41:25 -0700

    exlude class names from error matching

--- bin/matchStackTraceMessage.groovy
+++ bin/matchStackTraceMessage.groovy
@@ -7,10 +7,23 @@ def searchClos
 searchClos = {   it.eachFile {
                      if (it.name.endsWith(fileExtension)) {
                          def fileContents = it.getText()
+
                          if (textToMatch.contains("StackTrace")) {
                              textToMatch = textToMatch.substring(textToMatch.indexOf("StackTrace"), textToMatch.length())
                          }
-                         if (fileContents.contains(textToMatch)) {
+
+                         if (textToMatch.contains(" class ")) { // don't use classnames for error matching
+                            def preClass = textToMatch.substring(0, textToMatch.indexOf( " class "))
+                            def postClass = textToMatch.substring(textToMatch.indexOf(" class ") + 7, textToMatch.length())
+                            if (postClass.contains(" ")) {
+                                postClass = postClass.substring(postClass.indexOf(" "), postClass.length())                                
+                            } else {
+                                postClass == null
+                            }
+                            if (fileContents.contains(preClass) && (postClass == null || fileContents.contains(postClass))) {
+                                println(it.name)
+                            }
+                         } else if (fileContents.contains(textToMatch)) {
                              println(it.name)
                          }
                      } 
