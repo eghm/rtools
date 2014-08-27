be08bd91f4ee73dc43525dc14ae46fb3228747f3
Erik G. H. Meade <github-m@eghm.net>
2014-08-27 15:35:35 -0700

    jtl2Csv one build number at a time

---
+++ bin/loadtest/jtl2Csv.sh
@@ -0,0 +1,91 @@
+# R_HOME must be set to find $R_HOME/rtools/bin/loadtest/jtls.txt which defines jtl files and the key used to get the timings of just the page under test
+if [ -z "$R_HOME" ]
+then
+    echo "env R_HOME is not set!  Exiting."
+    exit
+fi
+
+# build number must be passed in
+if [ -z $1 ]
+then
+	echo "Jenkins build number of rice-2.5-test-jmeter-env19-nightly must be passed in"
+	exit
+fi
+
+# if an archive directory already exists stop
+if [ -d archive ]
+then
+	echo "archive directory already exists, please rename and try again"
+    exit
+fi
+
+# if an archive zip file already exists stop
+if [ -f "archive.zip" ] 
+then
+	echo "archive zip file already exists, please rename and try again"
+    exit
+fi
+
+# if there isn't a renamed archive file for the given build number get it
+if [ ! -f "rice-2.5-test-jmeter-env19-nightly-$1.zip" ] 
+then
+    wget --no-check-certificate https://ci.kuali.org/view/rice/view/2.5/view/performance/job/rice-2.5-test-jmeter-env19-nightly/$1/artifact/*zip*/archive.zip
+	mv archive.zip rice-2.5-test-jmeter-env19-nightly-$1.zip
+fi
+
+# if there isn't an unzipped directory make it
+if [ ! -d "rice-2.5-test-jmeter-env19-nightly-$1" ] 
+then
+    unzip rice-2.5-test-jmeter-env19-nightly-$1.zip
+    mv archive rice-2.5-test-jmeter-env19-nightly-$1
+fi
+
+cd rice-2.5-test-jmeter-env19-nightly-$1-archive
+
+rm -rf jtl.headers.txt
+
+
+# Setup pages as CSV file
+for f in $(cat $R_HOME/rtools/bin/loadtest/jtls.txt) ; do
+
+    JTL_FILE=$(echo $f | cut -d: -f1)   
+    JTL_PAGE=$(echo $f | cut -d: -f2)   
+
+    echo $(basename $JTL_FILE) >> jtl.headers.txt
+
+done 
+
+tr '\n' , < jtl.headers.txt > jtl.headers.csv
+#cat jtl.headers.csv
+#echo -e ""
+
+# Setup averages as CVS file
+rm -rf $41.ave.times
+for f in $(cat $R_HOME/rtools/bin/loadtest/jtls.txt) ; do
+
+    JTL_FILE=$(echo $f | cut -d: -f1)   
+    JTL_PAGE=$(echo $f | cut -d: -f2)
+
+	rm -rf $(basename $JTL_FILE).times
+
+# CI results are in a different location than if run locally
+#echo "grep lb=.$JTL_PAGE logs/results/$(basename $JTL_FILE) | cut -d\" -f2"
+    grep lb=.$JTL_PAGE logs/results/$(basename $JTL_FILE) | cut -d\" -f2 >> $(basename $JTL_FILE).times
+
+    awk '{s+=$1}END{print s/NR}' RS=" " $(basename $JTL_FILE).times >> $41.ave.times
+
+done 
+
+tr '\n' , < $41.ave.times > $41.ave.times.csv
+
+# Display
+echo -e "\n\n\n"
+cat jtl.headers.csv
+echo -e ""
+cat $41.ave.times.csv
+echo -e ""
+
+# Cleanup
+rm -rf jtl.headers.txt
+rm -rf jtl.headers.csv
+rm -rf *.times
