ab899490b98b1ac099148483b47b18cae94c46b0
Erik G. H. Meade <github-m@eghm.net>
2014-10-19 16:09:44 -0700

    browser testing rerun script

--- bin/browsertest/retestskrad.sh
+++ bin/browsertest/retestskrad.sh
@@ -10,15 +10,24 @@ export DTS=$(date +%Y%m%d%H%M)
 #/bin/bash -c "$R_HOME/rtools/bin/browsertest/saucelabsdownload.sh $R_HOME/$4 rice-framework/krad-sampleapp/web/"
 
 
-for f in $(ls $6/*.results) ; do
+for f in $(ls -1 $6/*.results) ; do
 
-    export TEST_CLASS=$(grep failed  $f | cut -d: -f 2 | cut -d' ' -f 1 | cut -d. -f 1)
-    export TEST_METHOD=$(grep failed  $f | cut -d: -f 2 | cut -d' ' -f 1 | cut -d. -f 2)
-
-    export SAUCE_OS=$(grep failed  *.results | cut -d- -f 2);
-    export SAUCE_BROWSER=$(grep failed  *.results | cut -d- -f 3);	
-    export SAUCE_VERSION=$(grep failed  *.results | cut -d- -f 4);
+    export SAUCE_OS=$(echo  $f | cut -d- -f 2);
+    export SAUCE_BROWSER=$(echo  $f | cut -d- -f 3);	
+    export SAUCE_VERSION=$(echo  $f | cut -d- -f 4);
 		
+    # TODO for each (there is 1 test method for each test class here
+    grep failed $f | cut -d' ' -f 1 > rerun.txt    
+
+    while [ -s rerun.txt ] 
+    do
+
+
+    export TEST_CLASS=$(tail -n 1 rerun.txt | cut -d. -f 1);
+    export TEST_METHOD=$(tail -n 1 rerun.txt | cut -d. -f 2);
+
+    echo "$TEST_CLASS#$TEST_METHOD";
+
     export logname=$1-$SAUCE_OS-$SAUCE_BROWSER-$SAUCE_VERSION-$5-$DTS
 
     touch ../logs/$4/$logname.out 
@@ -26,21 +35,23 @@ for f in $(ls $6/*.results) ; do
     date >> $logname.out
     mvn -version  >> $logname.out
     echo "MAVEN_OPTS=$MAVEN_OPTS" >> $logname.out
-    echo "mvn -f rice-framework/krad-sampleapp/web/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs -Dsaucelabs.user=$2 -Dsaucelabs.key=$3 -Dsaucelabs.browser.version=$SAUCE_VERSION -Dsaucelabs.platform=$SAUCE_OS -Dsaucelabs.browser=$SAUCE_BROWSER -Duser=$5 -Drice.version=$4 -Dremote.jgrowl.enabled=false -Dremote.driver.highlight=false -Dsaucelabs.download.scripts=true -Dit.test=$TEST $7 $8 $9 >> $logname.out"
-    ( mvn -f rice-framework/krad-sampleapp/web/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs -Dsaucelabs.user=$2 -Dsaucelabs.key=$3 -Dsaucelabs.browser.version=$SAUCE_VERSION -Dsaucelabs.platform=$SAUCE_OS -Dsaucelabs.browser=$SAUCE_BROWSER -Dremote.public.user=$5 -Drice.version=$4 -Dremote.jgrowl.enabled=false -Dremote.driver.highlight=false -Dsaucelabs.download.scripts=true -Dit.test=$TEST_CLASS#TEST_METHOD $7 $8 $9 >> $logname.out 2>&1 && touch /tmp/$logname )
+    echo "mvn -f rice-framework/krad-sampleapp/web/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs -Dsaucelabs.user=$2 -Dsaucelabs.key=$3 -Dsaucelabs.browser.version=$SAUCE_VERSION -Dsaucelabs.platform=$SAUCE_OS -Dsaucelabs.browser=$SAUCE_BROWSER -Duser=$5 -Drice.version=$4 -Dremote.jgrowl.enabled=false -Dremote.driver.highlight=false -Dsaucelabs.download.scripts=true -Dit.test=$TEST_CLASS#$TEST_METHOD $7 $8 $9 >> $logname.out"
+    ( mvn -f rice-framework/krad-sampleapp/web/pom.xml failsafe:integration-test -Pstests -Dremote.public.url=$1 -Dremote.driver.saucelabs -Dsaucelabs.user=$2 -Dsaucelabs.key=$3 -Dsaucelabs.browser.version=$SAUCE_VERSION -Dsaucelabs.platform=$SAUCE_OS -Dsaucelabs.browser=$SAUCE_BROWSER -Dremote.public.user=$5 -Drice.version=$4 -Dremote.jgrowl.enabled=false -Dremote.driver.highlight=false -Dsaucelabs.download.scripts=true -Dit.test=$TEST_CLASS#$TEST_METHOD $7 $8 $9 >> $logname.out 2>&1 && touch /tmp/$logname )
     date >> $logname.out
 
     while [ ! -e /tmp/$logname ]
     do
 	    sleep 10;
-    done
+    done;
     rm /tmp/$logname
+
+    sed '$d' rerun.txt > rerun.txt.cut
+    mv rerun.txt.cut rerun.txt	
+
+    done;
 	
-	sed '$d' $TESTS_FILE > $TESTS_FILE.cut
-    mv $TESTS_FILE.cut $TESTS_FILE	
 done;
 
 # sleep to give seperate download script chance to complete last file
 sleep 30 
 
-rm "$TESTS_FILE"
